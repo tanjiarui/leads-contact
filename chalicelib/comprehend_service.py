785c53fbd6389f77bf3955596a77a1558d1fc513
Terry <tanjiarui@hotmail.com>
2022-03-24 22:10:21 -0400

    fourth version modify identification recognition strategy based on the research result

--- chalicelib/comprehend_service.py
+++ chalicelib/comprehend_service.py
@@ -3,13 +3,24 @@ import boto3
 
 class Comprehend:
 	def __init__(self, access_key_id, secret_access_key):
-		self.client = boto3.client('comprehend', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+		self.comprehend = boto3.client('comprehend', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+		self.medical = boto3.client('comprehendmedical', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
 
 	def detect_pii(self, text_line):
-		text = ''
+		text = str()
 		for line in text_line:
 			text += line['text'] + '\n'
-		response = self.client.detect_pii_entities(Text=text, LanguageCode='en')
+		response = self.comprehend.detect_pii_entities(Text=text, LanguageCode='en')
+		result = dict()
+		for entity in response['Entities']:
+			result[entity['Type']] = text[entity['BeginOffset']:entity['EndOffset']]
+		return result
+
+	def detect_phi(self, text_line):
+		text = str()
+		for line in text_line:
+			text += line['text'] + '\n'
+		response = self.medical.detect_phi(Text=text)
 		result = dict()
 		for entity in response['Entities']:
 			result[entity['Type']] = text[entity['BeginOffset']:entity['EndOffset']]
