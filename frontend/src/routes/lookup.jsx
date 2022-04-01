7a2c57fbea0dc45f89fe98782e961efcbe4f185a
amneshia <amirshayan.armaghan@gmail.com>
2022-03-31 23:18:25 -0400

    Complete view/edit contact page

--- frontend/src/routes/lookup.jsx
+++ frontend/src/routes/lookup.jsx
@@ -1,11 +1,121 @@
+import React from 'react';
+import { Button, FormControl, Input, Typography, CircularProgress, FormGroup, TextField, alpha, Grid } from '@mui/material';
 import { Page } from "../components/page";
+import api from '../lib/api';
+import { makeStyles, useTheme } from '@mui/styles';
+import { useProcess } from '../lib/hooks';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import ContactForm from '../components/contactForm';
+import { Cancel, Delete, Edit, PhotoCamera } from '@mui/icons-material';
+
+const useStyles = makeStyles((theme) => ({
+    img: {
+        width: "100%"
+    }
+}))
+
+const Contact = ({ contact, onUpdated, onDeleted }) => {
+    console.log(contact)
+    const [mode, setMode] = React.useState('view')
+    const switchMode = () => {
+        setMode(m => m === "view" ? "edit" : "view")
+    }
+
+    const onDelete = async () => {
+        await api.Contact().delete(contact.id)
+            .then(() => onDeleted(contact.id))
+            .catch(console.log)
+    }
+
+    const theme = useTheme();
+    const headerText = mode === "view" ? "Contact Info" : "Edit Contact";
+    return (
+        <Paper component="div" sx={{
+            p: 2,
+            boxShadow: `0px 3px 1px -2px ${theme.palette.primary.light}, 0px 2px 2px 0px ${theme.palette.primary.light}, 0px 1px 5px 0px ${theme.palette.primary.light}`
+        }}
+        >
+            <Grid container direction={"row"}>
+                <Grid item xs>
+                    <Typography variant="h6" component="h6" display="block" gutterBottom>{headerText}</Typography>
+                </Grid>
+                <Grid item xs="auto">
+                    <IconButton component="span" onClick={switchMode}>
+                        {mode === "edit" ? <Cancel />: <Edit />}
+                    </IconButton>
+                </Grid>
+                <Grid item xs="auto">
+                    <IconButton color="error" component="span" onClick={onDelete}>
+                        <Delete />
+                    </IconButton>
+                </Grid>
+            </Grid>
+            {
+                mode === "edit" ?
+                    <ContactForm
+                        initialValues={{ ...contact, name: contact.username }}
+                        btnText={"Update"}
+                        onSubmit={(v) => api.Contact().update(contact.id, v)}
+                        onSuccess={(r) => {
+                            setMode("view")
+                            onUpdated(contact.id, r.Attributes)
+                        }}
+                    /> :
+                    <div>{contact.username}</div>
+            }
+        </Paper>
+    );
+}
 
 function LookupRoute() {
+    const [query, setQuery] = React.useState("")
+    const [contacts, setContacts] = React.useState([])
+    const onSubmitQuery = async (e) => {
+        e.preventDefault()
+        api.Contact().find(query)
+            .then(r => { console.log(r); return r })
+            .then(r => Array.isArray(r.data) ? r.data : [])
+            .then(setContacts)
+            .catch(console.log)
+    }
+    const onUpdated = React.useCallback((id, contact) => {
+        setContacts(cs => cs.map(c => c.id === id ? { ...contact, id: c.id } : c))
+    }, [])
+
+    const onDeleted = React.useCallback((id) => {
+        setContacts(cs => cs.filter(c => c.id !== id))
+    }, [])
+
+    const classes = useStyles()
+    const theme = useTheme()
     return (
         <Page>
-            <div>
-                TBD
-            </div>
+            <Typography variant="h6" component="h6" display="block" gutterBottom>Search Contacts</Typography>
+            <Paper
+                component="form"
+                sx={{ p: theme.spacing(0, 1), display: 'flex', alignItems: 'center', backgroundColor: alpha(theme.palette.primary.main, 0.05) }}
+                onSubmit={onSubmitQuery}
+            >
+                <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Search..."
+                    value={query}
+                    onChange={e => setQuery(e.target.value)}
+                />
+                <IconButton type="submit" sx={{ p: '10px' }} aria-label="search" onClick={onSubmitQuery}>
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
+            {contacts.length === 0 ? <></> : <Divider sx={{ mt: 2, mb: 2 }} />}
+            {
+                contacts.map(c =>
+                    <Contact key={c.id} contact={c} onUpdated={onUpdated} onDeleted={onDeleted}/>
+                )
+            }
         </Page>
     );
 }
--- frontend/src/routes/upload.jsx
+++ frontend/src/routes/upload.jsx
@@ -1,149 +1,18 @@
 import { PhotoCamera } from '@mui/icons-material';
-import { Button, FormControl, Input, Typography, CircularProgress, FormGroup, TextField } from '@mui/material';
+import { Button, FormControl, Input, Typography, CircularProgress, FormGroup, TextField, Divider } from '@mui/material';
 import React from 'react';
 import { Page } from "../components/page";
 import api from '../lib/api';
-import { makeStyles } from '@mui/styles';
+import { makeStyles, useTheme } from '@mui/styles';
 import { useProcess } from '../lib/hooks';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
 import { useNavigate } from 'react-router-dom';
+import ContactForm from '../components/contactForm';
 
 const useStyles = makeStyles((theme) => ({
     img: {
         width: "100%"
     }
 }))
-
-const formSchema = yup.object({
-    name: yup
-        .string("Enter contact's name")
-        .default('')
-        .required("Contact's name is required"),
-    email: yup
-        .string("Enter contact's email")
-        .default('')
-        .email('Enter a valid email'),
-    address: yup
-        .string("Enter contact's address")
-        .default('')
-        .nullable(),
-    phone: yup
-        .string("Enter contact's phone")
-        .default('')
-        .nullable(),
-    website: yup
-        .string("Enter contact's website url")
-        .default('')
-        .url("Enter a valid website url")
-});
-
-
-const Form = ({ imageId, initialValues, onCreate }) => {
-
-    const formik = useFormik({
-        initialValues: {
-            name: initialValues.name || "",
-            email: initialValues.email || "",
-            address: initialValues.address || "",
-            phone: initialValues.phone || "",
-            website: initialValues.website || "",
-        },
-        validationSchema: formSchema,
-        onSubmit: async (values) => {
-            await api.Contact().create(imageId, values)
-                .then(
-                    r => onCreate(imageId, r.data),
-                    r => {
-                        console.log(r)
-                        // formik.setErrors(r.data.errors)
-                    });
-            return true;
-        }
-    });
-
-    return (
-        <form onSubmit={formik.handleSubmit}>
-            <FormGroup>
-                <TextField //name
-                    id="outlined-name-input"
-                    label="Name"
-                    type="text"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    value={formik.values.name}
-                    onChange={formik.handleChange("name")}
-                    onBlur={formik.handleBlur("name")}
-                    error={formik.touched.name && Boolean(formik.errors.name)}
-                    helperText={formik.touched.name && formik.errors.name}
-                />
-                <TextField //email
-                    id="outlined-email-input"
-                    label="Email"
-                    type="email"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    value={formik.values.email}
-                    onChange={formik.handleChange("email")}
-                    onBlur={formik.handleBlur("email")}
-                    error={formik.touched.email && Boolean(formik.errors.email)}
-                    helperText={formik.touched.email && formik.errors.email}
-                />
-                <TextField //address
-                    id="outlined-address-input"
-                    label="Address"
-                    type="text"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    value={formik.values.address}
-                    onChange={formik.handleChange("address")}
-                    onBlur={formik.handleBlur("address")}
-                    error={formik.touched.address && Boolean(formik.errors.address)}
-                    helperText={formik.touched.address && formik.errors.address}
-                />
-                <TextField //phone
-                    id="outlined-phone-input"
-                    label="Phone"
-                    type="phone"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    value={formik.values.phone}
-                    onChange={formik.handleChange("phone")}
-                    onBlur={formik.handleBlur("phone")}
-                    error={formik.touched.phone && Boolean(formik.errors.phone)}
-                    helperText={formik.touched.phone && formik.errors.phone}
-                />
-                <TextField //website
-                    id="outlined-website-input"
-                    label="Website"
-                    type="text"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    value={formik.values.website}
-                    onChange={formik.handleChange("website")}
-                    onBlur={formik.handleBlur("website")}
-                    error={formik.touched.website && Boolean(formik.errors.website)}
-                    helperText={formik.touched.website && formik.errors.website}
-                />
-                <br />
-                <Button
-                    type="submit"
-                    disabled={formik.isSubmitting}
-                    onClick={formik.handleSubmit}
-                    variant="contained"
-                    color="primary"
-                    fullWidth>
-                    Create
-                </Button>
-            </FormGroup>
-        </form>
-    );
-}
 
 function UploadRoute() {
 
@@ -185,6 +54,7 @@ function UploadRoute() {
         }
     }, [imageInfo])
 
+    const theme = useTheme()
     const navigate = useNavigate();
     const classes = useStyles()
 
@@ -202,15 +72,20 @@ function UploadRoute() {
             {
                 imageInfo &&
                 <>
-                    <hr />
+                    <Divider sx={{ mt: 1, mb: 1 }} />
                     <img className={classes.img} src={imageInfo.fileUrl} />
                 </>
             }
             {
                 imageInfo && form &&
                 <>
-                    <hr />
-                    <Form imageId={imageInfo.fileId} initialValues={form} onCreate={() => navigate("/")} />
+                    <Divider sx={{ mt: 1, mb: 1 }} />
+                    <ContactForm
+                        initialValues={form}
+                        btnText={"Create"}
+                        onSubmit={(v) => api.Contact().create(imageInfo.fileId, v)}
+                        onSuccess={(r) => navigate("/")}
+                    />
                 </>
             }
         </Page>
